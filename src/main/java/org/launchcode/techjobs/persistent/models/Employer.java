d89010647fad6660217dd12399ec9ffe47ce72ae
Nisha George <nishajomy.2007@gmail.com>
2023-06-01 19:00:41 -0500

    modified Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;it
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
